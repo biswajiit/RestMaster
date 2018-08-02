ad180ed783c043acd11ee03723355258e7b95dfb
biswajiit <biswajit.jmishra@gmail.com>
2018-08-02 23:50:00 +0530

    commit

--- restMaster/src/main/java/com/restMaster/controller/HomeController.java
+++ restMaster/src/main/java/com/restMaster/controller/HomeController.java
@@ -1,15 +1,32 @@
 package com.restMaster.controller;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-	@PostMapping
+	@GetMapping("/test")
+	public ResponseEntity<String> test() {
+		return new ResponseEntity<String>("success", HttpStatus.OK);
+	}
+
+	@PostMapping("/template")
 	public ResponseEntity<String> createRestTemplate(HttpEntity<String> httpEntity) {
+		final String json = httpEntity.getBody();
+		try {
+			JSONObject obj = new JSONObject(json);
+			System.out.println("valid json");
+		} catch (final JSONException e) {
+			System.out.println("not a valid Json");
+			e.printStackTrace();
+		}
 
 		return null;
 	}
